12804e0ccbbb9b178a018e807ffc62c6fca5d67e
shintaniyuta <cada3986@yahoo.co.jp>
2023-07-09 09:35:13 +0900

    Add Unicorn and update Procfile

---
+++ config/unicorn.rb
@@ -0,0 +1,15 @@
+worker_processes 3
+timeout 30
+preload_app true
+
+before_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
