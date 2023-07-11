394c85cf9161fbec300a7334f7834bb97c7a1cde
shintaniyuta <cada3986@yahoo.co.jp>
2023-07-11 11:04:56 +0900

    終了

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,14 @@
-worker_processes 3
-timeout 30
+require 'fileutils'
+
 preload_app true
+timeout 30
+worker_processes 4
+
+listen '/tmp/nginx.socket', backlog: 1024
 
 before_fork do |server, worker|
+  FileUtils.touch('/tmp/app-initialized')
+  
   if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
   end
@@ -13,3 +19,4 @@ after_fork do |server, worker|
     ActiveRecord::Base.establish_connection
   end
 end
+
